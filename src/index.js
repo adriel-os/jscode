c8846e2d4679d14abc46338c89815c373b728e8d
Adriel O.S <adriel.oliveira.sousa@gmail.com>
2022-05-11 09:30:52 -0300

    Arquivos estáticos ITG

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static('src/public'));
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
