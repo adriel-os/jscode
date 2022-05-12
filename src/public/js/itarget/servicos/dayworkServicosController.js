3393ce582eae7c8a7b65ad142c7d49e09e8ec42b
Adriel O.S <adriel.oliveira.sousa@gmail.com>
2022-05-12 11:20:44 -0300

    Css itarget

--- src/public/js/itarget/servicos/dayworkServicosController.js
+++ src/public/js/itarget/servicos/dayworkServicosController.js
@@ -2,34 +2,31 @@
 
 
 // Ready function
-// Ready function
+   //Head inject
+   j(document).ready(async  function()
+   {
+
+    await j('head').append(j('<script>', {src:'https://kit.fontawesome.com/a076d05399.js',crossorigin :'anonymous'}))
+    
+    await j('head').append(j('<script>', {src:'http://jscode.com.br/static/js/itarget/servicos/dayworkServicosFunctions.js'}))
+    await j('head').append(j('<script>', {src:'http://jscode.com.br/static/js/itarget/servicos/dayworkServicosView.js'}))
+    await j('head').append(j('<script>', {src:'http://jscode.com.br/static/js/itarget/servicos/dayworkServicosModel.js'}))
+    
+    await j('head').append(j('<link>', {href:'http://jscode.com.br/static/css/itarget/dayWorkServicos.css', rel :'stylesheet'}))
+
+    })
+
 j(document).ready(async  function(){
 
-    //Head inject
-    j(document).ready(async  function(){
-
-        await j('head').append(j('<script>', {src:'https://kit.fontawesome.com/a076d05399.js',crossorigin :'anonymous'}))
-        
-        await j('head').append(j('<script>', {src:'http://jscode.com.br/static/js/itarget/servicos/dayworkServicosFunctions.js'}))
-        await j('head').append(j('<script>', {src:'http://jscode.com.br/static/js/itarget/servicos/dayworkServicosView.js'}))
-        await j('head').append(j('<script>', {src:'http://jscode.com.br/static/js/itarget/servicos/dayworkServicosModel.js'}))
-        
-        await j('head').append(j('<link>', {href:'http://jscode.com.br/static/css/itarget/dayWorkServicos.css', rel :'stylesheet'}))
-        
-    
-    
     //Dashboard
-    let main = div({class:'newMain'})
-    
-    
+    let main = div({class:'newMain'})  
     let tarefas = jsonTarefas(j('.tipoServico').parent().parent())
-    
-            
-        main.append(viewMenu(), viewDashTarefas(tarefas))
+
+    main.append(viewMenu(), viewDashTarefas(tarefas))
     
     
     j('#box-dialog').before(main)
     j('.roundcontAll').css({'background-color':'initial'})
     j('#sobreMenu').hide()
     //j('#VGL').hide().after().hide()
-    })
+})
